e389d6cb9ef66521c0112cf055303b2c51b22413
Tanushabsc <tanusham.bsc23@rvu.edu.in>
2024-02-21 19:12:30 +0530

    test

--- set02/problem04.c
+++ set02/problem04.c
@@ -1,6 +1,54 @@
 //Write a program to find Sum of composite number in an array of different numbers entered by the user.
 #include <stdio.h>
 int input_array_size()
+{
+    int n;
+    printf("Enter Size of array: ");
+    scanf("%d",&n);
+    return n;
+}
 void input_array(int n, int a[n])
+{
+    printf("Enter the elements:\n");
+    for(int i=0;i<n;i++){
+    scanf("%d",&a[i]);
+    }
+    
+}
 int sum_composite_numbers(int n, int a[n])
+{
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        int result=0;
+        for(int j=1;j<=a[i];j++)
+        {
+            if(a[i]%j==0){
+            result+=1;
+            }
+        }
+    
+        if(result>2){
+        sum+=a[i];
+        }
+    }
+    return sum;
+    
+}
 void output(int sum)
+{
+    printf("The sum is %d", sum);
+}
+
+int main()
+{
+    int n;
+    n=input_array_size();
+    int a[n];
+    input_array(n,a);
+    int s = sum_composite_numbers(n,a);
+    output(s);
+    return 0;
+
+}
+
--- set02/problem05.c
+++ set02/problem05.c
@@ -1,1 +1,5 @@
 //Write a program to find GCD (HCF) of two numbers.
+#include <stdio.h>
+int input();
+int find_gcd(int a, int b);
+void output(int a, int b, int gcd)
