2151675712d536cff13d32fbba258d992792ea1b
Tanushabsc <tanusham.bsc23@rvu.edu.in>
2024-02-22 19:05:26 +0530

    test

--- set02/problem05.c
+++ set02/problem05.c
@@ -1,5 +1,34 @@
 //Write a program to find GCD (HCF) of two numbers.
 #include <stdio.h>
-int input();
-int find_gcd(int a, int b);
+int input()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    return n;
+}
+int find_gcd(int a, int b)
+{
+    int gcd;
+    for(int i=1;i<=a && i<=b ;i++)
+    {
+        if(a%i==0 && b%i==0)
+        {
+            gcd=i;
+        }
+    }
+    return gcd;
+}
 void output(int a, int b, int gcd)
+{
+    printf("GCD of %d and %d is %d",a,b,gcd);
+}
+
+int main()
+{
+    int a=input();
+    int b=input();
+    int gcd = find_gcd(a,b);
+    output(a,b,gcd);
+    return 0;
+}
---
+++ set02/problem07.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+typedef struct _triangle {
+	float base, altitude, area;
+} Triangle;
+
+Triangle input_triangle()
+{
+    Triangle t;
+    printf("Enter base of a triangle: ");
+    scanf("%f",&t.base);
+    printf("Enter altitude of a triangle: ");
+    scanf("%f",&t.altitude);
+    return t;
+
+}
+void find_area(Triangle *t)
+{
+    t->area= 0.5 * (t->base * t->altitude);
+}
+void output(Triangle t)
+{
+    printf("The area of triangle is %f",t.area);
+}
+
+int main()
+{
+    Triangle t = input_triangle();
+    find_area(&t);
+    output(t);
+    return 0;
+}
